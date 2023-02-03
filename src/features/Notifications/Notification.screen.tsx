9c1e3849031a7709d34654f5bfdba458a96b4d8c
John Valle <jvalle@Johns-MacBook-Air.local>
2023-02-03 14:42:18 +0800

    updated notification page scroll

--- src/features/Notifications/Notification.screen.tsx
+++ src/features/Notifications/Notification.screen.tsx
@@ -2,29 +2,22 @@ import { Image, ScrollView } from "react-native";
 import { Dimensions } from "react-native";
 
 import { Box, Text, Wrapper } from "@core/components";
-import theme from "@core/constants/theme";
 
 import NotificationPageBanner from "@assets/images/notification-page.png";
 
+import styles from "./Notification.styles";
 import NotificationCard from "./components/NotificationCard";
 
 const Notifications = () => {
-  const paddedDeviceHeight = Dimensions.get("window").height - theme.spacing.lg;
-  const imgHeight = 200;
-  const scrollViewHeight = paddedDeviceHeight - imgHeight;
-  const h = scrollViewHeight;
-  const d = (h / paddedDeviceHeight) * 100;
-
-  console.log({ h, d: d });
   return (
     <Wrapper>
-      <Image source={NotificationPageBanner} style={{ width: 300, height: 200 }} resizeMode="contain" />
+      <Image source={NotificationPageBanner} style={styles.bannerImage} resizeMode="contain" />
       <Text variant="mediumBold" color="black" marginVertical="md">
         Notifications
       </Text>
-      <Box>
-        <ScrollView style={{ width: "100%" }}>
-          <Box flexDirection="column" gap="2xs" width="100%">
+      <Box maxHeight={styles.scrollViewContainer.height} width="100%">
+        <ScrollView>
+          <Box flexDirection="column" gap="2xs">
             <NotificationCard read={false} type="info" />
             <NotificationCard read={true} type="warning" />
             <NotificationCard read={true} type="danger" />
