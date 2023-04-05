c5d03f43cc94c9e5792aee7e8b87b45cc3124aa2
johnvalle <vallejohn.personal@gmail.com>
2023-04-05 21:55:07 +0800

    Add new packages to android

--- android/app/src/main/java/com/gasulerto/MainApplication.java
+++ android/app/src/main/java/com/gasulerto/MainApplication.java
@@ -24,6 +24,7 @@ public class MainApplication extends Application implements ReactApplication {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
+          packages.add(new me.pushy.sdk.react.PushyPackage());
           // packages.add(new MyReactNativePackage());
           return packages;
         }
