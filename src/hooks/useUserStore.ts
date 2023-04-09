b2af021231e6e296fbdf11ffa40ef334e386cc42
johnvalle <vallejohn.personal@gmail.com>
2023-04-09 21:52:57 +0800

    Add threshold to store

--- src/hooks/useUserStore.ts
+++ src/hooks/useUserStore.ts
@@ -13,27 +13,29 @@ type User = {
   token: Nullable<string>;
   userId: Nullable<string>;
   isAnonymous: boolean;
+  threshold: Nullable<number>;
 };
 
 export type UserStore = {
-  setUser: (user: User) => void;
+  setUser: (user: Partial<User>) => void;
   logOut: () => void;
 } & User;
 
-const defaultState = { name: null, expiresOn: null, userId: null, token: null, isAnonymous: false };
+const defaultState = { name: null, expiresOn: null, userId: null, threshold: null, token: null, isAnonymous: false };
 
 export const useUserStore = create<UserStore>()(
   persist(
     set => ({
       ...defaultState,
-      setUser: (user: User) => set(user),
+      setUser: (user: Partial<User>) => set(state => ({ ...state, ...user })),
       logOut: () => {
         set({
           name: null,
           expiresOn: null,
           token: null,
           userId: null,
-          isAnonymous: false
+          isAnonymous: false,
+          threshold: null
         });
       }
     }),
